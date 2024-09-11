3ccee5751724e0a2f6c030c36ab697eb53e9a682
Seyed Ali Sanei <ssanei@3-F-8.42heilbronn.de>
2024-09-11 19:38:26 +0200

    init_builtin_cmd was compeleted and developed

--- is_builtin_command.c
+++ is_builtin_command.c
@@ -6,13 +6,31 @@
 /*   By: ssanei <ssanei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 17:06:12 by ssanei            #+#    #+#             */
-/*   Updated: 2024/09/08 18:06:28 by ssanei           ###   ########.fr       */
+/*   Updated: 2024/09/11 19:28:01 by ssanei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	find_command_index(const char *command_name, t_biltin_c commands[],
+void	init_builtin_cmd(t_builtin_c b_commands[])
+{
+	b_commands[0].name = "cd";
+	b_commands[0].function = ft_cd;
+	b_commands[1].name = "echo";
+	b_commands[1].function = ft_echo;
+	b_commands[2].name = "export";
+	b_commands[2].function = ft_export;
+	b_commands[3].name = "unset";
+	b_commands[3].function = ft_unset;
+	b_commands[4].name = "pwd";
+	b_commands[4].function = ft_pwd;
+	b_commands[5].name = "env";
+	b_commands[5].function = ft_env;
+	b_commands[6].name = "exit";
+	b_commands[6].function = ft_exit;
+}
+
+int	find_command_index(const char *command_name, t_builtin_c commands[],
 		int num_commands)
 {
 	int	i;
@@ -31,21 +49,13 @@ int	find_command_index(const char *command_name, t_biltin_c commands[],
 
 bool	is_builtin_command(char **argv)
 {
-	t_biltin_c	commands[NUM_B_CMD];
+	t_builtin_c	commands[NUM_B_CMD];
 	int			command_index;
 
-	commands = init_biltin_cmd();
+	init_builtin_cmd(commands);
 	command_index = find_command_index(argv[0], commands, NUM_B_CMD);
 	if (command_index != -1)
 		return (true);
-	printf("Command not found: %s\n", argv[0]);
+	printf(CMD_N_F " %s\n", argv[0]);
 	return (false);
 }
-
-// t_biltin_c	init_biltin_cmd(void)
-// {
-// 	t_biltin_c	commands[NUM_B_CMD];
-// 	commands[0].name = "cd";
-    
-// 	return (commands);
-// }
